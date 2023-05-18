a2e247072a1e60e96acce3aac6401abc7bf277b4
tukuhuse <tukuhuse@icloud.com>
2023-05-19 02:16:41 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+const env = process.env;
+
+app.listen(env.PORT, () => {
+	console.log(`server is running port ${env.PORT}`);
+});
+
+// 健康診断の結果を表示
+// 健康診断の結果を登録
+// 健康診断の結果を修正
+// 健康診断の結果を削除
+app.get('/', (req, res) => {
+	console.log("Hello");
+	res.send("Hello");
+});
