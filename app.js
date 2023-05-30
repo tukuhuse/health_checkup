abc804d381e41b758fe66081fa5ae7bc91f5e0b2
tukuhuse <tukuhuse@icloud.com>
2023-05-31 06:55:34 +0900

    診断結果を登録する機能 #4

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require('dotenv').config();
+const env = process.env;
+
 const connectDB = require('./utils/database');
 const HealthCheckup = require('./utils/schemaModels');
 
@@ -17,6 +20,17 @@ app.get('/showall', async(req, res) => {
 	console.log("success show all");
 });
 // 健康診断の結果を登録
+app.post('/items', async(req, res) => {
+	try {
+		const healthData = req.body;
+
+		connectDB();
+		await HealthCheckup.create(healthData);
+		return res.status(200).json({message: "データ登録成功"});
+	} catch {
+		return res.status(400).json({message: "データ登録失敗"});
+	}
+});
 // 健康診断の結果を修正
 // 健康診断の結果を削除
 app.get('/', async(req, res) => {
