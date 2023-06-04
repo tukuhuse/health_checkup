440a160e259a123fd9430ade673ee6c7a81ef637
tukuhuse <tukuhuse@icloud.com>
2023-06-04 14:44:44 +0900

    コード整形 #4

--- app.js
+++ app.js
@@ -13,11 +13,19 @@ app.listen(env.PORT, () => {
 	console.log(`server is running port ${env.PORT}`);
 });
 
-// 健康診断の結果を表示
-app.get('/showall', async(req, res) => {
-	connectDB();
-	await HealthCheckup.find();
-	console.log("success show all");
+// 健康診断の結果を表示(all)
+app.get('/items', async(req, res) => {
+	try {
+		connectDB();
+
+		const findData = await HealthCheckup.find();
+		return res.status(200).json({
+			message: "データ取得成功",
+			HealthData: findData
+		});
+	} catch {
+		return res.status(400).json({message:"データ取得失敗"});
+	}
 });
 // 特定の診断結果を表示
 app.get('/item/:id', async(req, res) => {
@@ -35,13 +43,16 @@ app.get('/item/:id', async(req, res) => {
 	}
 });
 // 健康診断の結果を登録
-app.post('/items', async(req, res) => {
+app.post('/item', async(req, res) => {
 	try {
 		const healthData = req.body;
 
 		connectDB();
-		await HealthCheckup.create(healthData);
-		return res.status(200).json({message: "データ登録成功"});
+		const createData = await HealthCheckup.create(healthData);
+		return res.status(200).json({
+			message: "データ登録成功",
+			HealthData: createData
+		});
 	} catch {
 		return res.status(400).json({message: "データ登録失敗"});
 	}
@@ -53,27 +64,27 @@ app.put('/item/:id', async(req, res) => {
 		const healthData = req.body;
 
 		connectDB();
-		console.log(updateId);
-		await HealthCheckup.updateOne({ _id: updateId }, { $set: healthData });
-		return res.status(200).json({message: "データ更新成功"});
+		const updateData = await HealthCheckup.updateOne({ _id: updateId }, { $set: healthData });
+		return res.status(200).json({
+			message: "データ更新成功",
+			HealthData: updateData
+		});
 	} catch {
 		return res.status(400).json({message: "データ更新失敗"});
 	}
-})
+});
 // 健康診断の結果を削除
 app.delete('/item/:id', async(req, res) => {
 	try {
 		const deleteId = req.params.id;
 
 		connectDB();
-		await HealthCheckup.deleteOne({ _id: deleteId });
-		return res.status(200).json({message: "データ削除成功"});
+		const deleteData = await HealthCheckup.deleteOne({ _id: deleteId });
+		return res.status(200).json({
+			message: "データ削除成功",
+			HealthData: deleteData
+		});
 	} catch {
 		return res.status(400).json({message: "データ削除失敗"});
 	}
-});
-app.get('/', async(req, res) => {
-	await mongoose.connect(env.MONGOOSE);
-	console.log("Hello");
-	res.send("Hello");
 });
--- utils/database.js
+++ utils/database.js
@@ -5,9 +5,7 @@ const env = process.env;
 const connectDB = async() => {
 	try {
 		await mongoose.connect(env.MONGOOSE, {useNewUrlParser: true});
-		console.log("Success: Connected to MongoDB");
 	} catch(err) {
-		console.log("Failure: UnConnected to MongoDB");
 		throw new Error();
 	}
 }
